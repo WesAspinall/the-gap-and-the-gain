bb6a856570160fb0781cad7db2f06521dfb06d09
Wes Aspinall <wra427@gmail.com>
2025-03-22 23:19:26 -0400

    added shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@/components/ui/button"
+import { useState } from "react"
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const increment = () => setCount(count + 1)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <p className="text-center pb-4">
+          Clicked {count} times
         </p>
+        <Button onClick={()=>increment()}>Click me</Button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
